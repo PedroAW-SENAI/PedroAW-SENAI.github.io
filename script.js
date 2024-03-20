e04a9f001aaa96c7a9a2005146b8e83a985fd678
PedroAW-SENAI <164074036+PedroAW-SENAI@users.noreply.github.com>
2024-03-19 21:54:31 -0300

    /

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const botaoSidebarLeft = document.querySelector('[button-sidebar-left]');
+const botaoSidebarRight = document.querySelector('[button-sidebar-right]');
+const sidebarCompleta = document.querySelector('[sidebarCompleta]');
+const sidebarReduzida = document.querySelector('[sidebarReduzida]');
+const sidebarContainer = document.querySelector('[sidebar]');
+
+const cabecalho = document.querySelector('[cabecalho]');
+
 const botaoSobreDown = document.querySelector('[button-sobre-down]');
 const botaoSobreUp = document.querySelector('[button-sobre-up]');
 
@@ -9,6 +17,33 @@ const botaoContatoUp = document.querySelector('[button-contato-up]');
 
 const listaContato = document.querySelector('[dropdown-contato]');
 const linhaContato = document.querySelector('[linha-contato]');
+
+botaoSidebarLeft.onclick = () => {
+    aparecer(botaoSidebarRight);
+    desaparecer(botaoSidebarLeft);
+    desaparecer(sidebarCompleta);
+    aparecer(sidebarReduzida);
+    cabecalho.style.width = "90vw";
+    cabecalho.style.marginLeft = "3vw";
+    sidebarReduzida.style.paddingLeft = "15px";
+    sidebarContainer.style.minWidth = "70px";
+    sidebarContainer.style.width = "70px";
+    sidebarContainer.style.alignItems = "center";
+    sidebarContainer.style.paddingLeft = "0px";
+};
+
+botaoSidebarRight.onclick = () => {
+    desaparecer(botaoSidebarRight);
+    aparecer(botaoSidebarLeft);
+    aparecer(sidebarCompleta);
+    desaparecer(sidebarReduzida);
+    cabecalho.style.width = "80vw";
+    cabecalho.style.marginLeft = "250px";
+    sidebarContainer.style.minWidth = "250px";
+    sidebarContainer.style.width = "250px";
+    sidebarContainer.style.alignItems = "flex-start";
+    sidebarContainer.style.paddingLeft = "20px";
+};
 
 botaoSobreDown.onclick = () => {
     aparecer(listaSobre);
