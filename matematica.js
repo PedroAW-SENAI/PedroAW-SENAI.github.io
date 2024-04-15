4b16498b2c8ef5e1f3f5cd1454996ece2d047461
PedroAW-SENAI <164074036+PedroAW-SENAI@users.noreply.github.com>
2024-04-15 16:54:41 -0300

    /*/*/*

---
+++ matematica.js
@@ -0,0 +1,4 @@
+const tituloTopicoMatematica = document.getElementById("topico-titulo-dropdown-topico-matematica").clientWidth;
+const conteudoTopicoMatematica = document.getElementById("topico-conteudo-dropdown-topico-matematica");
+
+conteudoTopicoMatematica.style.width = tituloTopicoMatematica.clientWidth + "px";
